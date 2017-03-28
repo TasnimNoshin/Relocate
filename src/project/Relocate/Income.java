d9c1e3e8a57318fed574b919a7d4c2b6403209a0
Tasnim Noshin <noshint@mcmaster.ca>
2017-03-27 20:28:21 -0400

    Add Income.java

---
+++ src/project/Relocate/Income.java
@@ -0,0 +1,55 @@
+package project.Relocate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Income {
+
+	ArrayList<CityIncome> cities;
+
+	public Income() throws IOException {
+		this.cities = new ArrayList<CityIncome>();
+		CityData[] cityData = (new Parser()).getCityDataArray();
+		toCityIncome(cityData);
+	}
+
+	// transfer CityData to CityIncome
+	public void toCityIncome(CityData[] c) {
+		int index;
+		for (CityData cd : c) {
+			CityIncome cI = new CityIncome(cd.getCity());
+			index = searchCity(cd.getCity());
+			if (index == -1) {
+				// adds income to the city
+				cI.addIncome(cd.getIncome());
+				// adds city to cities
+				cities.add(cI);
+			} else {
+				// if city already exists add income to the city
+				CityIncome temp = cities.get(index);
+				temp.addIncome(cd.getIncome());
+				cities.set(index, temp);
+			}
+		}
+	}
+
+	// search and get the index of the city, if not found return -1
+	public int searchCity(String cityName) {
+		if (this.cities.contains(cityName)) {
+			for (int i = 0; i < this.cities.size(); i++) {
+				if (this.cities.get(i).equals(cityName)) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Income n = new Income();
+		for(CityIncome e : n.cities){
+			System.out.println(e.getCityName());
+		}
+	}
+}
